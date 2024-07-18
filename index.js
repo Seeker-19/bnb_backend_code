650aff1700a9c3f2ad2d22ad0203d1e54593149b
Anurag <kanswalanurag123@gmail.com>
2024-07-18 14:37:47 +0530

    done

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://keen-toffee-c88627.netlify.app/",
+    origin: "https://keen-toffee-c88627.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
