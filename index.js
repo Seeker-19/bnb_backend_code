ef5f361bb6832fd2f6abdbca588b5ac86c89317a
Anurag <kanswalanurag123@gmail.com>
2024-07-18 14:25:53 +0530

    done

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.FRONT_END_URL,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
