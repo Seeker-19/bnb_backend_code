3850fc7d7c80233ad28391029ba344ba928bcc49
Anurag <kanswalanurag123@gmail.com>
2024-09-12 00:17:59 +0530

    done

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://bnb-frontend-code-89rv.vercel.app/",
+    origin: "https://bnb-frontend-code-89rv.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
